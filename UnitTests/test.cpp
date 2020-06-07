e9c9bb907ab98cf93eaef1885a1a4bfde5e9ed38
Mikhail Natalenko <Kenjyminamori@gmail.com>
2020-06-07 11:50:11 +0300

    Add tests

--- UnitTests/test.cpp
+++ UnitTests/test.cpp
@@ -1,6 +1,21 @@
 #include "pch.h"
+#include "../tree_with_various_data_types/crc/Node.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseName, MirrorTest) {
+  
+    Node<int> a(3);
+    a.add_child(std::make_shared<Node<int>>(2));
+    a.add_child(std::make_shared<Node<float>>(3.1415));
+
+    nlohmann::json j;
+    a.append_json(j);
+    std::string s = j.dump();
+
+            
+    nlohmann::json mirror = nlohmann::json::parse(s);
+    
+    EXPECT_EQ(mirror[0]["data"], 3);
+    EXPECT_EQ(mirror[0]["arr"][0]["data"], 2);
 }
+
+
