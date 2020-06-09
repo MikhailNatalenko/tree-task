89d5dba82973f9cec744ba220a7fcbcbdf50b7cf
Mikhail Natalenko <Kenjyminamori@gmail.com>
2020-06-09 13:23:24 +0300

    Add args parse

--- tree_with_various_data_types/crc/main.cpp
+++ tree_with_various_data_types/crc/main.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>
 #include <nlohmann/json.hpp>
 #include "Node.h"
+//#include <boost/program_options.hpp>
 
+//namespace po = boost::program_options;
 
 void test_memleaks()
 {
@@ -39,9 +41,83 @@ void test_memleaks()
     }
 }
 
+#include <boost/program_options.hpp>
 
-int main()
+namespace po = boost::program_options;
+
+int main(int ac, char* av[])
 {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("input,i", po::value<std::string>(), "input file")
+        ("output,o", po::value<std::string>(), "output file")
+        ;
+
+    po::variables_map vm;
+    try {
+
+    po::store(po::parse_command_line(ac, av, desc), vm);
+    }
+    catch (po::unknown_option e)
+    {
+        std::cout << e.what() << std::endl;
+        std::cout << desc << std::endl;
+    }
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
+    if (vm.count("input")) {
+        std::cout << "Input file: "
+            << vm["input"].as<std::string>() << std::endl;
+    }
+    else {
+        std::cout << "Input file was not set." << std::endl;
+    }
+
+    if (vm.count("output")) {
+        std::cout << "output file: "
+            << vm["output"].as<std::string>() << std::endl;
+    }
+    else {
+        std::cout << "output file was not set.\n";
+    }
+    //try {
+
+    //    po::options_description desc("Allowed options");
+    //    desc.add_options()
+    //        ("help", "produce help message")
+    //        ("compression", po::value<double>(), "set compression level")
+    //        ;
+
+    //    po::variables_map vm;
+    //    po::store(po::parse_command_line(ac, av, desc), vm);
+    //    po::notify(vm);
+
+    //    if (vm.count("help")) {
+    //        std::cout << desc << "\n";
+    //        return 0;
+    //    }
+
+    //    if (vm.count("compression")) {
+    //        std::cout << "Compression level was set to "
+    //            << vm["compression"].as<double>() << ".\n";
+    //    }
+    //    else {
+    //        std::cout << "Compression level was not set.\n";
+    //    }
+    //}
+    //catch (std::exception & e) {
+    //    std::cerr << "error: " << e.what() << "\n";
+    //    return 1;
+    //}
+    //catch (...) {
+    //    std::cerr << "Exception of unknown type!\n";
+    //}
     //test_memleaks();
     std::cout << "Hello " << std::endl;
     treetask::Node<int> a(3);
